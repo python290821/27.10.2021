78fe827a9f15848d43ebcf7269825355cb0773d0
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-27 21:29:38 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,35 +1,55 @@
 
-
-#          0         1       len: 2
-#_l1 = [ [1,2,3,4], [5,6,7] ]
-#for i in range(len(_l1)):
-#    for j in range(len(_l1[i])):
-#        print(_l1[i][j])
-
-# rows = 3
-# columns = 3
-print('welcome to theater designer')
-_rows = int(input('how many rows? ')) #3
-_columns = int(input('how many seats in each row? '))
-_seats = [ ]
-
-
-for i in range(_rows):
-    one_row = []
-    for j in range(_columns):
-        one_row.append(0)
-    _seats.append(one_row)
-
-for i in range(len(_seats)):
-    for j in range(len(_seats[i])):
-        print(f'{{{_seats[i][j]}}}', end=' ')
+def print_cinema():
+    print('    ', end='')
+    for i in range(len(_seats[0])):
+        print (i,end='   ')
     print()
+    for i in range(len(_seats)):
+        print(f'{i}: ', end='')
+        for j in range(len(_seats[i])):
+            print(f'{{{_seats[i][j]}}}', end=' ')
+        print()
 
-# purchase tickets
-# ask for row, ask for column
-# 1: 0, 0
-# 2: 0,0 --- NO! occupied!
+def validate_row_col(_lst):
+    if len(_lst) != 2 or _lst[0].isdigit() == False \
+            or _lst[1].isdigit() == False:
+        print('illegal')
+        return False
+
+    r = int(_lst[0])
+    c = int(_lst[1])
+
+    if r >= 0 and c >= 0 and r <= _rows and c <= _columns:
+        if _seats[r][c] == 0:
+            return True
+        else:
+            print('seat occupied, try again')
+            return False
+    else:
+        print('illegal seat number')
+        return False
 
 
+def main():
+    print('welcome to theater designer')
+    _rows = int(input('how many rows? '))
+    _columns = int(input('how many seats in each row? '))
+    _seats = []
 
+    for i in range(_rows):
+        one_row = []
+        for j in range(_columns):
+            one_row.append(0)
+        _seats.append(one_row)
 
+    for counter in range(_rows * _columns):
+        while True:
+            print_cinema()
+            _lst = input(('please enter row and columns (with space): ')).split()
+            if validate_row_col(_lst):
+                break
+        r = int(_lst[0])
+        c = int(_lst[1])
+        _seats[r][c] = 1
+
+main()
